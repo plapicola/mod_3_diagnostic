c363cb48ec8878a8e5ac586b4d61e6034cf18b07
plapicola <p.lapicola@gmail.com>
2019-03-27 09:48:51 -0600

    Adds basic SearchFacade implementation

---
+++ app/facades/search_facade.rb
@@ -0,0 +1,14 @@
+class SearchFacade
+  def initialize(zipcode)
+    @zipcode = zipcode
+    @nrel_service = NRELService.new
+  end
+
+  def stations
+    
+  end
+
+  def total_station_count
+
+  end
+end
