43f77f5b70daad246a206837cae6b5318dfce728
plapicola <p.lapicola@gmail.com>
2019-03-27 11:06:05 -0600

    Corrects hardcoded zipcode

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -1,6 +1,6 @@
 class NRELService
   def nearest_stations(zipcode)
-    url = "/api/alt-fuel-stations/v1/nearest.json?location=80208&radius=5&status=E&access=public&fuel_type=ELEC,LPG&api_key=#{ENV['NREL_API_KEY']}"
+    url = "/api/alt-fuel-stations/v1/nearest.json?location=#{zipcode}&radius=5&status=E&access=public&fuel_type=ELEC,LPG&api_key=#{ENV['NREL_API_KEY']}"
     @nearest_stations ||= get_json(url)
   end
 
